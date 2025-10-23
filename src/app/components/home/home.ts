f7316bb8383dd351624df20690be9f65053c86b3
Edoardo <edoardo@timelapselab.com>
2025-10-23 23:20:19 +0200

    update font

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './home.css'
 })
 export class Home {
+
+  constructor(
+    private router: Router
+  ){}
+
+  navigate(route: string){
+    this.router.navigate([route]);
+  }
 
 }
